17c419d768861732b2f62b364176b9d3ef695e73
Diego Hernandez <diegofootball19@gmail.com>
2022-07-21 00:22:33 -0500

    added descrption to dogs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { StrictMode } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams.jsx";
@@ -9,7 +9,9 @@ const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <h1> adopt Me! </h1>
+        <header>
+          <Link to="/"></Link>
+        </header>
         <Routes>
           <Route path="/details/:id" element={<Details />}></Route>
           <Route path="/" element={<SearchParams />}></Route>
